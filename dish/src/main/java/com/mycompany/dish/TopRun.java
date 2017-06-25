0052e852cac0e394c4dbdaf6a1cd1ffff552d9b9
Li You <liyou@lis-imac.attlocal.net>
2017-06-25 11:43:32 -0500

    Update four square api token

--- dish/src/main/java/com/mycompany/dish/TopRun.java
+++ dish/src/main/java/com/mycompany/dish/TopRun.java
@@ -21,6 +21,8 @@ import java.util.Set;
  */
 public class TopRun {
 
+    String apiToken = "3HN5ZNTIV4EBCK5SVFJVFCHIJCBVVI4Q5CFJPYIPXLRUW0Y0&v=20170618";
+
     public List<Restaurant> getRestaurantList() throws IOException {
 
         JsonParser parser = new JsonParser();
@@ -29,9 +31,10 @@ public class TopRun {
         for (int offset = 0; offset < 241; offset += 30) {
 
             String file = String.format("./rawJson/RawJsonRestaurants_offset_%d.txt", offset);
-            String link = String.format("https://api.foursquare.com/v2/venues/explore?near=Austin,TX&section=Food&offset=%d&oauth_token=3HN5ZNTIV4EBCK5SVFJVFCHIJCBVVI4Q5CFJPYIPXLRUW0Y0&v=20161230", offset);
-//            String restaurantJSON = parser.readFromUrl(link, file);
-            String restaurantJSON = parser.readFromFile(file);
+
+            String link = String.format("https://api.foursquare.com/v2/venues/explore?near=Austin,TX&section=Food&offset=%d&oauth_token=%s", offset, apiToken);
+            String restaurantJSON = parser.readFromUrl(link, file);
+//            String restaurantJSON = parser.readFromFile(file);
 
             List<Restaurant> cur = parser.parseRestaurant(restaurantJSON);
 
@@ -45,26 +48,26 @@ public class TopRun {
 
         return restaurantList;
     }
-    
+
     public Map<String, Map<DishType, Integer>> countDish(List<Restaurant> restaurantList) throws IOException {
-        
+
         JsonParser parser = new JsonParser();
-        
+
         // Query menus of each reataurant
         for (int i = 0; i < restaurantList.size(); i++) {
             Restaurant cur = restaurantList.get(i);
 
-//            	String menuLink = String.format("https://api.foursquare.com/v2/venues/%s/menu?oauth_token=3HN5ZNTIV4EBCK5SVFJVFCHIJCBVVI4Q5CFJPYIPXLRUW0Y0&v=20161230", r.id);
+            String menuLink = String.format("https://api.foursquare.com/v2/venues/%s/menu?oauth_token=%s", cur.id, apiToken);
             String menuFile = "./rawJson/Menu_" + cur.name + ".txt";
 
-//            	parser.readFromUrl(menuLink, menuFile, jsonMenu);
-            String menuJSON = parser.readFromFile(menuFile);
+            String menuJSON = parser.readFromUrl(menuLink, menuFile);
+//            String menuJSON = parser.readFromFile(menuFile);
             Menus menus = parser.parseMenu(menuJSON);
 
             List<Dish> dishList = menus.flaten();
             cur.addDishes(dishList);
         }
-        
+
         // Count summary of all dishes
         Map<String, Map<DishType, Integer>> counter = new HashMap<>();
         for (Restaurant r : restaurantList) {
@@ -82,7 +85,7 @@ public class TopRun {
                 }
             }
         }
-        
+
         // Write counter to file
         PrintWriter out = new PrintWriter("./restaurantList/DishCount.txt");
         for (String d : counter.keySet()) {
@@ -103,7 +106,7 @@ public class TopRun {
         }
         return set;
     }
-    
+
     Map<String, List<Frequency>> countSummary(Map<String, Map<DishType, Integer>> counter) throws IOException {
         Map<String, List<Frequency>> summary = new HashMap<>();
         for (String cur : counter.keySet()) {
@@ -119,16 +122,16 @@ public class TopRun {
                 }
             }
         }
-        
+
         Comparator<Frequency> freqComparator = new Comparator<Frequency>() {
             @Override
             public int compare(Frequency op1, Frequency op2) {
                 return op2.freq - op1.freq;
             }
         };
-        
+
         PrintWriter out = new PrintWriter("./restaurantList/SortedDishCount.txt");
-        
+
         for (String k : summary.keySet()) {
             summary.get(k).sort(freqComparator);
             out.println(k + ' ' + summary.get(k));
@@ -143,35 +146,35 @@ public class TopRun {
         List<Restaurant> restaurantList = top.getRestaurantList();
 
         Map<String, Map<DishType, Integer>> counter = top.countDish(restaurantList);
-        
+
         Map<String, List<Frequency>> sorted = top.countSummary(counter);
 
-       
+
         System.out.println(String.format("\nTotal %d restaurants.\n", restaurantList.size()));
 
         System.out.println(counter.size() + " dishes in total.\n");
 
         System.out.println(sorted.size() + " keys in total.\n");
-        
-        
-//        JedisMain test = new JedisMain();
-        
+
+
+        JedisMain test = new JedisMain();
+
 //        System.out.println("\nRedis build up begins.\n");
 //        test.build(sorted);
 //        System.out.println("Redis build up completes.\n");
-//        String key = "fr";
-//        System.out.println(key + ":");
-//        System.out.println(test.query(key, 10));
-        
-        
-        
-        DatabaseAndSearchConnect conn = new DatabaseAndSearchConnect("test", "127.0.0.1", "austin", true);
-        conn.cleanup();
-        conn.close();
+        String key = "pot";
+        System.out.println(key + ":");
+        System.out.println(test.query(key, 10));
 
-        DatabaseAndSearchConnect connet = new DatabaseAndSearchConnect("test", "127.0.0.1", "austin", true);
-        connet.InitOrUpdate(restaurantList);
-        connet.close();
+
+
+//        DatabaseAndSearchConnect conn = new DatabaseAndSearchConnect("test", "127.0.0.1", "austin", true);
+//        conn.cleanup();
+//        conn.close();
+//
+//        DatabaseAndSearchConnect connet = new DatabaseAndSearchConnect("test", "127.0.0.1", "austin", true);
+//        connet.InitOrUpdate(restaurantList);
+//        connet.close();
 
         System.out.println("excution finished");
         final String dir = System.getProperty("user.dir");
